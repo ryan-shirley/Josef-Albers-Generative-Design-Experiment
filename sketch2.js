79fe7e0fe7ae2e6a57d195e5871a612b2ae23e16
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-28 15:34:46 +0000

    Init drawing postion
    Able to change whole drawing position

--- sketch2.js
+++ sketch2.js
@@ -1,7 +1,15 @@
-'use strict';
+"use strict"
 
 class Line {
-    constructor(colour, width, height, vertSpacing, amount, startPosX, startPosY) {
+    constructor(
+        colour,
+        width,
+        height,
+        vertSpacing,
+        amount,
+        startPosX,
+        startPosY
+    ) {
         this.colour = colour
         this.height = height
         this.width = width
@@ -14,19 +22,17 @@ class Line {
     draw() {
         // Draw
         let yPos = this.startPosY
-        for(let i = 0; i < this.amount; i++) {
+        for (let i = 0; i < this.amount; i++) {
             push()
             translate(this.startPosX * colWidth, yPos * rowHeight)
-    
+
             fill(this.colour)
-            rect(0, 0, this.width * colWidth, this.height * rowHeight);
+            rect(0, 0, this.width * colWidth, this.height * rowHeight)
             pop()
 
             // Add height of line + vertical spacing
             yPos += this.height + this.vertSpacing
         }
-        
-        
     }
 }
 
@@ -39,11 +45,14 @@ let lineHeight1 = 1
 let lineHeight2 = lineHeight1 * 3
 
 // Grid
-let gridSize = 300
-let numCol = gridSize;
-let numRows = gridSize;
-let colWidth;
-let rowHeight;
+let gridSize = 250
+let numCol = gridSize
+let numRows = gridSize
+let colWidth
+let rowHeight
+
+// Painting Start Position
+let initX, initY
 
 // Lines
 let line1, line2, line3, line5, line6
@@ -63,8 +72,12 @@ function setup() {
     colour2 = color(0)
 
     // Get grid values
-    colWidth = width/numCol;
-    rowHeight = height/numRows;
+    colWidth = width / numCol
+    rowHeight = height / numRows
+
+    // Init drawing position
+    initX = 20
+    initY = 60
 
     //  Draw Grid
     // fill(217, 106, 19)
@@ -78,7 +91,6 @@ function setup() {
     //     }
     // }
 
-
     // ***************************************************************************
     // Think Black lines
     // --
@@ -88,41 +100,57 @@ function setup() {
     let amount = [6, 8, 10, 12]
 
     // Line 2 - Small
-    let [startPos] = blackGrouping.splice(Math.floor(Math.random() * blackGrouping.length), 1)
+    let [startPos] = blackGrouping.splice(
+        Math.floor(Math.random() * blackGrouping.length),
+        1
+    )
 
-    line2 = new Line(colour2, 8, lineHeight2, 7, 1, startPos, 1)
-    for(let i = 1; i <= 3; i++) {
+    line2 = new Line(colour2, 8, lineHeight2, 7, 1, startPos + initX, 1 + initY)
+    for (let i = 1; i <= 3; i++) {
         let [amt] = amount.splice(Math.floor(Math.random() * amount.length), 1)
         line2.amount = amt
 
-        line2.startPosX += i !== 0 ? line2.width + i * 8 : 0
-        line2.startPosY += i == 1 ? (line2.height + line2.vertSpacing) * 2 : i === 2 && - (line2.height + line2.vertSpacing)
+        line2.startPosX += i !== 1 && line2.width + i * 8
+        line2.startPosY +=
+            i == 2
+                ? (line2.height + line2.vertSpacing) * 2
+                : i === 3 && -(line2.height + line2.vertSpacing)
 
         line2.draw()
 
         // ***************************************************************************
         // Thin white line middle of think black grouping
         // ***************************************************************************
-        if(i === 2) {
-            line6 = new Line(colour1, line2.width * 2, lineHeight1, 9, line2.amount - 4, line2.startPosX - line2.width, 35)
+        if (i === 2) {
+            line6 = new Line(
+                colour1,
+                line2.width * 2,
+                lineHeight1,
+                9,
+                line2.amount - 4,
+                line2.startPosX - line2.width + initX,
+                35 + initY
+            )
             line6.draw()
         }
     }
 
     // Line 1 - Large
-    let [start] = blackGrouping.splice(Math.floor(Math.random() * blackGrouping.length), 1)
+    let [start] = blackGrouping.splice(
+        Math.floor(Math.random() * blackGrouping.length),
+        1
+    )
     let [amt] = amount.splice(Math.floor(Math.random() * amount.length), 1)
 
     let l1largeStart = start === 58 ? start + 60 + line2.width : start
 
-    line1 = new Line(colour2, 12, lineHeight2, 7, amt, l1largeStart, 1)
+    line1 = new Line(colour2, 12, lineHeight2, 7, amt, l1largeStart + initX, 1 + initY)
     line1.draw()
-
 
     // ***************************************************************************
     // Thin Black line
     // ***************************************************************************
-    line3 = new Line(colour2, 200, lineHeight1, 9, 8, 1, 7)
+    line3 = new Line(colour2, 200, lineHeight1, 9, 8, 1 + initX, 7 + initY)
     line3.draw()
 
     // ***************************************************************************
@@ -130,11 +158,11 @@ function setup() {
     // ***************************************************************************
     // Line 5
     let l5Start =
-      start === 2
-        ? line1.startPosX + line1.width + line2.width
-        : line1.startPosX - 36;
+        start === 2
+            ? line1.startPosX + line1.width + line2.width
+            : line1.startPosX - 36
 
-    line5 = new Line(colour1, 20, lineHeight1, 9, line1.amount - 5, l5Start, 33)
+    line5 = new Line(colour1, 20, lineHeight1, 9, line1.amount - 5, l5Start, 33 + initY)
     line5.draw()
 
     // Second line 5 in gap
